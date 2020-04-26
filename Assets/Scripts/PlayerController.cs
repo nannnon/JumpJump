114211b063345e00dcf5fa5417ad4fbde0f91518
Koba <koba@dummy.com>
2020-04-26 17:58:49 +0900

    ためジャンプをできるようにした

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,12 +7,16 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody m_rigidBody;
     private Slider m_jumpingGauge;
+    private bool m_previousJumpButton;
+    private float m_jumpPower;
 
     // Start is called before the first frame update
     void Start()
     {
         m_rigidBody = GetComponent<Rigidbody>();
         m_jumpingGauge = GameObject.Find("JumpingGauge").GetComponent<Slider>();
+        m_previousJumpButton = false;
+        m_jumpPower = 0;
     }
 
     // Update is called once per frame
@@ -30,14 +34,39 @@ public class PlayerController : MonoBehaviour
             m_rigidBody.MovePosition(m_rigidBody.position + new Vector3(0.1f, 0, 0));
         }
 
-        const float kHitRadius = 0.5f;
-        Vector3 belowFoot = this.transform.position + new Vector3(0, -0.1f - kHitRadius, 0);
-        bool isGrounded = Physics.CheckSphere(belowFoot, kHitRadius);
-
-        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
         {
-            const float kJumpForce = 400;
-            m_rigidBody.AddForce(new Vector3(0, kJumpForce, 0));
+            const float kHitRadius = 0.5f;
+            Vector3 belowFoot = this.transform.position + new Vector3(0, -0.1f - kHitRadius, 0);
+            bool isGrounded = Physics.CheckSphere(belowFoot, kHitRadius);
+
+            if (isGrounded)
+            {
+                bool jumpButton = Input.GetKey(KeyCode.Space);
+
+                if (jumpButton)
+                {
+                    const float kJumpPowerUnit = 0.03f;
+                    m_jumpPower += kJumpPowerUnit;
+                    if (m_jumpPower >= 1)
+                    {
+                        m_jumpPower = 0;
+                    }
+
+                    m_jumpingGauge.value = m_jumpPower;
+                }
+                else if (m_previousJumpButton)
+                {
+                    const float kMaxJumpForce = 400;
+                    float jumpForce = kMaxJumpForce * m_jumpPower;
+                    m_rigidBody.AddForce(new Vector3(0, jumpForce, 0));
+                }
+
+                m_previousJumpButton = jumpButton;
+            }
+            else
+            {
+                m_jumpPower = 0;
+            }
         }
     }
 }
