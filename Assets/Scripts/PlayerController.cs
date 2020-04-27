a72672825174a0b09fc01582e4121f889561e444
Koba <koba@dummy.com>
2020-04-27 16:05:28 +0900

    ホバリングを実装した

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,8 @@ public class PlayerController : MonoBehaviour
     private Slider m_jumpingGauge;
     private bool m_previousJumpButton;
     private float m_jumpPower;
+    private Slider m_hoveringGauge;
+    private float m_hoveringPower;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +19,8 @@ public class PlayerController : MonoBehaviour
         m_jumpingGauge = GameObject.Find("JumpingGauge").GetComponent<Slider>();
         m_previousJumpButton = false;
         m_jumpPower = 0;
+        m_hoveringGauge = GameObject.Find("HoveringGauge").GetComponent<Slider>();
+        m_hoveringPower = 1;
     }
 
     // Update is called once per frame
@@ -36,16 +40,16 @@ public class PlayerController : MonoBehaviour
             m_rigidBody.MovePosition(m_rigidBody.position + new Vector3(0.1f, 0, 0));
         }
 
-        // ためジャンプ
         {
             const float kHitRadius = 0.5f;
             Vector3 belowFoot = this.transform.position + new Vector3(0, -0.1f - kHitRadius, 0);
             bool isGrounded = Physics.CheckSphere(belowFoot, kHitRadius);
 
+            bool jumpButton = Input.GetKey(KeyCode.Space);
+
+            // ためジャンプ
             if (isGrounded)
             {
-                bool jumpButton = Input.GetKey(KeyCode.Space);
-
                 if (jumpButton)
                 {
                     const float kJumpPowerUnit = 0.03f;
@@ -63,13 +67,32 @@ public class PlayerController : MonoBehaviour
                     float jumpForce = kMaxJumpForce * m_jumpPower;
                     m_rigidBody.AddForce(new Vector3(0, jumpForce, 0));
                 }
-
-                m_previousJumpButton = jumpButton;
             }
             else
             {
                 m_jumpPower = 0;
             }
+
+            // ホバリング
+            if (isGrounded)
+            {
+                if (m_hoveringPower != 1)
+                {
+                    m_hoveringGauge.value = 1;
+                }
+                m_hoveringPower = 1;
+            }
+            else if (jumpButton && m_hoveringPower > 0)
+            {
+                const float kHoveringPowerUnit = 0.01f;
+                m_hoveringPower -= kHoveringPowerUnit;
+                m_hoveringGauge.value = m_hoveringPower;
+
+                const float kHoveringForce = 10;
+                m_rigidBody.AddForce(new Vector3(0, kHoveringForce, 0));
+            }
+
+            m_previousJumpButton = jumpButton;
         }
     }
 }
